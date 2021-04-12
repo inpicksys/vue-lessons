80403f12c0bfe057d6e2de599fa88aa39313b5f8
Misha <michael.s@tailornova.com>
2021-04-12 08:37:37 +0300

    vue js all examples

---
+++ cmp-communication-assignment-problem/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,43 @@
+<template>
+     <li>
+        <h2>{{ name}} </h2>
+        <h3> {{age}}  </h3>
+    </li>
+</template>
+<script>
+export default {
+  props:{
+     id: {
+         type: Number,
+         required: true
+     },
+     name: {
+        type: String,
+        required: true
+     },
+     age: {
+       type: String,
+       required: true
+     }
+     
+  },
+  emits: ['delete'],
+data() {
+      return {
+          name: '',
+          age: ''
+      };
+  },
+  methods: {
+        // toggleDetails() {
+        //     this.detailsAreVisible = !this.detailsAreVisible;
+        // },
+        // toggleFavorite() {
+        //     this.$emit('toggle-favorite', this.id);
+        // },
+        // deleteFriend() {
+        //     this.$emit('delete', this.id);
+        // }
+    }
+};
+</script>
